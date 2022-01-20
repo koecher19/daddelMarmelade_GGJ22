098b1b5f28be0505c8f9f63643ec37a9086ef1d2
koecher19 <ida.koecher@gmail.com>
2022-01-20 17:05:48 +0100

    sidescroller test sprites + simple player movement

---
+++ Assets/Scripts/TestPlayer_Move_Prot.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer_Move_Prot : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    private bool facingRight = false;
+    public int playerJumpPower = 1;
+    private float moveX;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMove();
+    }
+
+    void playerMove()
+    {
+        // TODO: controls
+        moveX = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump();
+        }
+        // TODO: animations
+        // TODO: player directions
+        if (moveX < 0.0f && facingRight == false)
+        {
+            flipPlayer();
+        }else if(moveX > 0.0f && facingRight == true)
+        {
+            flipPlayer();
+        }
+        // TODO: physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    void flipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
