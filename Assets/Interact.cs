4834986ad7d57413625fa943fdba15429d721792
koecher19 <ida.koecher@gmail.com>
2022-01-23 18:09:11 +0100

    can now use inventory items on interactable

--- Assets/Interact.cs
+++ Assets/Interact.cs
@@ -9,6 +9,8 @@ public class Interact : MonoBehaviour
     public GameObject textbox;      // panel
     public TMP_Text description;    // text
     public TextAsset textfile;      // textfile contains description
+    public GameObject inventory;    // players inventory
+    public List<GameObject> usableItems;
 
     // Start is called before the first frame update
     void Start()
@@ -27,13 +29,48 @@ public class Interact : MonoBehaviour
     // hover mouse over this object
     void OnMouseOver()
     {
-        // TODO: either dispaly description or use inventory
-        textbox.SetActive(true);
+        // if we have a description: display it
+        if(this.description != null)
+        {
+            textbox.SetActive(true);
+
+        }
     }
+
+    // when you stop hovering the mouse over object
     void OnMouseExit()
     {
         // stop dislplaying description
-        textbox.SetActive(false);
+        if(this.description != null)
+        {
+            textbox.SetActive(false);
+        }
+    }
+
+    // click on item
+    void OnMouseDown()
+    {
+        // use inventory on item
+        useItem();
+    }
+
+    void useItem()
+    {
+        // check if inventory contains object that is stored as usable item for this object
+        // if yes: delete from usableItem list and use item
+        foreach(GameObject item in this.usableItems)
+        {
+            // check if you have a usable item in inventory
+            if (inventory.GetComponent<Inventory>().items.Contains(item))
+            {
+                // if yes: dele item from usableItem list
+                this.usableItems.Remove(item);
+                // use item on this object
+                inventory.GetComponent<Inventory>().useItem(item);
+                // to use other item you have to click again
+                break;
+            }
+        }
     }
 
 }
